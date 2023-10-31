87c7a9c40e09122d1ebf7e18abde12a35bb94c03
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-10-31 09:15:47 -0400

    Nice-u

--- src/TempConverter.java
+++ src/TempConverter.java
@@ -1,5 +1,56 @@
-public class Main {
+import java.util.Scanner;
+import java.lang.Math;
+public class TempConverter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double tempCelsius = 0;
+        double tempFahr = 0;
+        boolean valid = false;
+        boolean n = false;
+        String yesOrNo = "";
+
+        while (!n) {
+            System.out.println("Please enter the value for Celsius that you would like to be converted to Fahrenheit.");
+
+            do {
+                if (scan.hasNextDouble()) {
+                    tempCelsius = scan.nextDouble();
+
+                    //To Clear Buffer
+                    scan.nextLine();
+
+                    tempFahr = tempCelsius * (9 / 5) + 32;
+                    System.out.printf("The Fahrenheit is : %.2f", tempFahr);
+                    System.out.printf("\nThe Celsius is : %.2f", tempCelsius);
+
+                    System.out.println("\nWould you like to add another temperature to be converted? Y/N.");
+                    yesOrNo = scan.nextLine();
+                    if (yesOrNo.equalsIgnoreCase("N")){
+                        n = true;
+                    } else {
+                        n = false;
+                    }
+
+                    valid = true;
+                } else {
+                    scan.nextLine();
+                    System.out.println("You have entered an invalid temperature. Please try again.");
+                    valid = false;
+
+                }
+
+            } while (!valid);
+
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
